HorizenLabs/blockscout-frontend

fixes and tests

ui/address/tokenSelect/TokenSelect.tsx
@@ -5,7 +5,6 @@ import NextLink from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
 
-import type { SocketMessage } from 'lib/socket/types';
 import type { Address } from 'types/api/address';
 
 import walletIcon from 'icons/wallet.svg';
@@ -13,8 +12,6 @@ import { getResourceKey } from 'lib/api/useApiQuery';
 import useIsMobile from 'lib/hooks/useIsMobile';
 import * as mixpanel from 'lib/mixpanel/index';
 import getQueryParamString from 'lib/router/getQueryParamString';
-import useSocketChannel from 'lib/socket/useSocketChannel';
-import useSocketMessage from 'lib/socket/useSocketMessage';
 
 import useFetchTokens from '../utils/useFetchTokens';
 import TokenSelectDesktop from './TokenSelectDesktop';
@@ -28,14 +25,13 @@ const TokenSelect = ({ onClick }: Props) => {
   const router = useRouter();
   const isMobile = useIsMobile();
   const queryClient = useQueryClient();
-  const [ blockNumber, setBlockNumber ] = React.useState<number>();
 
   const addressHash = getQueryParamString(router.query.hash);
   const addressResourceKey = getResourceKey('address', { pathParams: { hash: addressHash } });
 
   const addressQueryData = queryClient.getQueryData<Address>(addressResourceKey);
 
-  const { data, isError, isLoading, refetch } = useFetchTokens({ hash: addressQueryData?.hash });
+  const { data, isError, isLoading } = useFetchTokens({ hash: addressQueryData?.hash });
   const tokensResourceKey = getResourceKey('address_tokens', { pathParams: { hash: addressQueryData?.hash }, queryParams: { type: 'ERC-20' } });
   const tokensIsFetching = useIsFetching({ queryKey: tokensResourceKey });
 
@@ -43,34 +39,6 @@ const TokenSelect = ({ onClick }: Props) => {
     mixpanel.logEvent(mixpanel.EventTypes.PAGE_WIDGET, { Type: 'Tokens show all (icon)' });
     onClick?.();
   }, [ onClick ]);
-
-  const handleTokenBalanceMessage: SocketMessage.AddressTokenBalance['handler'] = React.useCallback((payload) => {
-    if (payload.block_number !== blockNumber) {
-      refetch();
-      setBlockNumber(payload.block_number);
-    }
-  }, [ blockNumber, refetch ]);
-  const handleCoinBalanceMessage: SocketMessage.AddressCoinBalance['handler'] = React.useCallback((payload) => {
-    if (payload.coin_balance.block_number !== blockNumber) {
-      refetch();
-      setBlockNumber(payload.coin_balance.block_number);
-    }
-  }, [ blockNumber, refetch ]);
-
-  const channel = useSocketChannel({
-    topic: `addresses:${ addressQueryData?.hash.toLowerCase() }`,
-    isDisabled: !addressQueryData,
-  });
-  useSocketMessage({
-    channel,
-    event: 'coin_balance',
-    handler: handleCoinBalanceMessage,
-  });
-  useSocketMessage({
-    channel,
-    event: 'token_balance',
-    handler: handleTokenBalanceMessage,
-  });
 
   if (isLoading) {
     return (

luxfi/explore

ui/address/tokenSelect/TokenSelect.tsx
@@ -5,7 +5,6 @@ import NextLink from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
 
-import type { SocketMessage } from 'lib/socket/types';
 import type { Address } from 'types/api/address';
 
 import walletIcon from 'icons/wallet.svg';
@@ -13,8 +12,6 @@ import { getResourceKey } from 'lib/api/useApiQuery';
 import useIsMobile from 'lib/hooks/useIsMobile';
 import * as mixpanel from 'lib/mixpanel/index';
 import getQueryParamString from 'lib/router/getQueryParamString';
-import useSocketChannel from 'lib/socket/useSocketChannel';
-import useSocketMessage from 'lib/socket/useSocketMessage';
 
 import useFetchTokens from '../utils/useFetchTokens';
 import TokenSelectDesktop from './TokenSelectDesktop';
@@ -28,14 +25,13 @@ const TokenSelect = ({ onClick }: Props) => {
   const router = useRouter();
   const isMobile = useIsMobile();
   const queryClient = useQueryClient();
-  const [ blockNumber, setBlockNumber ] = React.useState<number>();
 
   const addressHash = getQueryParamString(router.query.hash);
   const addressResourceKey = getResourceKey('address', { pathParams: { hash: addressHash } });
 
   const addressQueryData = queryClient.getQueryData<Address>(addressResourceKey);
 
-  const { data, isError, isPending, refetch } = useFetchTokens({ hash: addressQueryData?.hash });
+  const { data, isError, isPending } = useFetchTokens({ hash: addressQueryData?.hash });
   const tokensResourceKey = getResourceKey('address_tokens', { pathParams: { hash: addressQueryData?.hash }, queryParams: { type: 'ERC-20' } });
   const tokensIsFetching = useIsFetching({ queryKey: tokensResourceKey });
 
@@ -43,34 +39,6 @@ const TokenSelect = ({ onClick }: Props) => {
     mixpanel.logEvent(mixpanel.EventTypes.PAGE_WIDGET, { Type: 'Tokens show all (icon)' });
     onClick?.();
   }, [ onClick ]);
-
-  const handleTokenBalanceMessage: SocketMessage.AddressTokenBalance['handler'] = React.useCallback((payload) => {
-    if (payload.block_number !== blockNumber) {
-      refetch();
-      setBlockNumber(payload.block_number);
-    }
-  }, [ blockNumber, refetch ]);
-  const handleCoinBalanceMessage: SocketMessage.AddressCoinBalance['handler'] = React.useCallback((payload) => {
-    if (payload.coin_balance.block_number !== blockNumber) {
-      refetch();
-      setBlockNumber(payload.coin_balance.block_number);
-    }
-  }, [ blockNumber, refetch ]);
-
-  const channel = useSocketChannel({
-    topic: `addresses:${ addressQueryData?.hash.toLowerCase() }`,
-    isDisabled: !addressQueryData,
-  });
-  useSocketMessage({
-    channel,
-    event: 'coin_balance',
-    handler: handleCoinBalanceMessage,
-  });
-  useSocketMessage({
-    channel,
-    event: 'token_balance',
-    handler: handleTokenBalanceMessage,
-  });
 
   if (isPending) {
     return (