Usman01234/frontend

Zapper integration

configs/app/features/multichainButton.ts
@@ -1,22 +1,24 @@
 import type { Feature } from './types';
-import type { MultichainProviderConfig } from 'types/client/multichainProviderConfig';
+import type { MultichainProviderConfig, MultichainProviderConfigResult } from 'types/client/multichainProviderConfig';
 
 import { getEnvValue, parseEnvJson } from '../utils';
 import marketplace from './marketplace';
 
-const value = parseEnvJson<MultichainProviderConfig>(getEnvValue('NEXT_PUBLIC_MULTICHAIN_BALANCE_PROVIDER_CONFIG'));
+const value = parseEnvJson<Array<MultichainProviderConfig>>(getEnvValue('NEXT_PUBLIC_MULTICHAIN_BALANCE_PROVIDER_CONFIG'));
 
 const title = 'Multichain balance';
 
-const config: Feature<{name: string; logoUrl?: string; urlTemplate: string; dappId?: string }> = (() => {
+const config: Feature<{ providers: Array<MultichainProviderConfigResult> }> = (() => {
   if (value) {
     return Object.freeze({
       title,
       isEnabled: true,
-      name: value.name,
-      logoUrl: value.logo,
-      urlTemplate: value.url_template,
-      dappId: marketplace.isEnabled ? value.dapp_id : undefined,
+      providers: value.map((provider) => ({
+        name: provider.name,
+        logoUrl: provider.logo,
+        urlTemplate: provider.url_template,
+        dappId: marketplace.isEnabled ? provider.dapp_id : undefined,
+      })),
     });
   }
 

types/client/multichainProviderConfig.ts
@@ -2,5 +2,12 @@ export type MultichainProviderConfig = {
   name: string;
   dapp_id?: string;
   url_template: string;
-  logo?: string;
+  logo: string;
+};
+
+export type MultichainProviderConfigResult = {
+  name: string;
+  logoUrl: string;
+  urlTemplate: string;
+  dappId?: string;
 };

ui/address/details/AddressMultichainButton.tsx
@@ -1,0 +1,70 @@
+import { Image, Tooltip } from '@chakra-ui/react';
+import _capitalize from 'lodash/capitalize';
+import React from 'react';
+
+import type { MultichainProviderConfigResult } from 'types/client/multichainProviderConfig';
+
+import { route } from 'nextjs-routes';
+
+import LinkExternal from 'ui/shared/links/LinkExternal';
+import LinkInternal from 'ui/shared/links/LinkInternal';
+
+const TEMPLATE_ADDRESS = '{address}';
+
+type Props = {
+  hasSingleProvider: boolean;
+  item: MultichainProviderConfigResult;
+  addressHash: string;
+  onClick?: () => void;
+}
+
+const AddressMultichainButton = ({ item, addressHash, onClick, hasSingleProvider }: Props) => {
+
+  const buttonIcon = <Image src={ item.logoUrl } alt={ item.name } boxSize={ 5 } mr={ hasSingleProvider ? 2 : 0 } borderRadius="4px" overflow="hidden"/>;
+
+  const buttonContent = hasSingleProvider ? (
+    <>
+      { buttonIcon }
+      { _capitalize(item.name) }
+    </>
+  ) : (
+    <Tooltip label={ _capitalize(item.name) }>{ buttonIcon }</Tooltip>
+  );
+
+  const linkProps = {
+    variant: hasSingleProvider ? 'subtle' as const : undefined,
+    display: 'flex',
+    alignItems: 'center',
+    fontSize: 'sm',
+    lineHeight: 5,
+    fontWeight: 500,
+    onClick,
+  };
+
+  try {
+    const portfolioUrlString = item.urlTemplate.replace(TEMPLATE_ADDRESS, addressHash);
+    const portfolioUrl = new URL(portfolioUrlString);
+    portfolioUrl.searchParams.append('utm_source', 'blockscout');
+    portfolioUrl.searchParams.append('utm_medium', 'address');
+    const dappId = item.dappId;
+    return typeof dappId === 'string' ? (
+      <LinkInternal
+        href={ route({ pathname: '/apps/[id]', query: { id: dappId, url: portfolioUrl.toString() } }) }
+        { ...linkProps }
+      >
+        { buttonContent }
+      </LinkInternal>
+    ) : (
+      <LinkExternal
+        href={ portfolioUrl.toString() }
+        { ...linkProps }
+      >
+        { buttonContent }
+      </LinkExternal>
+    );
+  } catch (error) {}
+
+  return null;
+};
+
+export default AddressMultichainButton;

luxfi/explore

configs/app/features/multichainButton.ts
@@ -1,22 +1,24 @@
 import type { Feature } from './types';
-import type { MultichainProviderConfig } from 'types/client/multichainProviderConfig';
+import type { MultichainProviderConfig, MultichainProviderConfigParsed } from 'types/client/multichainProviderConfig';
 
 import { getEnvValue, parseEnvJson } from '../utils';
 import marketplace from './marketplace';
 
-const value = parseEnvJson<MultichainProviderConfig>(getEnvValue('NEXT_PUBLIC_MULTICHAIN_BALANCE_PROVIDER_CONFIG'));
+const value = parseEnvJson<Array<MultichainProviderConfig>>(getEnvValue('NEXT_PUBLIC_MULTICHAIN_BALANCE_PROVIDER_CONFIG'));
 
 const title = 'Multichain balance';
 
-const config: Feature<{name: string; logoUrl?: string; urlTemplate: string; dappId?: string }> = (() => {
+const config: Feature<{ providers: Array<MultichainProviderConfigParsed> }> = (() => {
   if (value) {
     return Object.freeze({
       title,
       isEnabled: true,
-      name: value.name,
-      logoUrl: value.logo,
-      urlTemplate: value.url_template,
-      dappId: marketplace.isEnabled ? value.dapp_id : undefined,
+      providers: value.map((provider) => ({
+        name: provider.name,
+        logoUrl: provider.logo,
+        urlTemplate: provider.url_template,
+        dappId: marketplace.isEnabled ? provider.dapp_id : undefined,
+      })),
     });
   }
 

types/client/multichainProviderConfig.ts
@@ -2,5 +2,12 @@ export type MultichainProviderConfig = {
   name: string;
   dapp_id?: string;
   url_template: string;
-  logo?: string;
+  logo: string;
+};
+
+export type MultichainProviderConfigParsed = {
+  name: string;
+  logoUrl: string;
+  urlTemplate: string;
+  dappId?: string;
 };

ui/address/details/AddressMultichainButton.tsx
@@ -1,0 +1,70 @@
+import { Image, Tooltip } from '@chakra-ui/react';
+import _capitalize from 'lodash/capitalize';
+import React from 'react';
+
+import type { MultichainProviderConfigParsed } from 'types/client/multichainProviderConfig';
+
+import { route } from 'nextjs-routes';
+
+import LinkExternal from 'ui/shared/links/LinkExternal';
+import LinkInternal from 'ui/shared/links/LinkInternal';
+
+const TEMPLATE_ADDRESS = '{address}';
+
+type Props = {
+  hasSingleProvider: boolean;
+  item: MultichainProviderConfigParsed;
+  addressHash: string;
+  onClick?: () => void;
+}
+
+const AddressMultichainButton = ({ item, addressHash, onClick, hasSingleProvider }: Props) => {
+
+  const buttonIcon = <Image src={ item.logoUrl } alt={ item.name } boxSize={ 5 } mr={ hasSingleProvider ? 2 : 0 } borderRadius="4px" overflow="hidden"/>;
+
+  const buttonContent = hasSingleProvider ? (
+    <>
+      { buttonIcon }
+      { _capitalize(item.name) }
+    </>
+  ) : (
+    <Tooltip label={ _capitalize(item.name) }>{ buttonIcon }</Tooltip>
+  );
+
+  const linkProps = {
+    variant: hasSingleProvider ? 'subtle' as const : undefined,
+    display: 'flex',
+    alignItems: 'center',
+    fontSize: 'sm',
+    lineHeight: 5,
+    fontWeight: 500,
+    onClick,
+  };
+
+  try {
+    const portfolioUrlString = item.urlTemplate.replace(TEMPLATE_ADDRESS, addressHash);
+    const portfolioUrl = new URL(portfolioUrlString);
+    portfolioUrl.searchParams.append('utm_source', 'blockscout');
+    portfolioUrl.searchParams.append('utm_medium', 'address');
+    const dappId = item.dappId;
+    return typeof dappId === 'string' ? (
+      <LinkInternal
+        href={ route({ pathname: '/apps/[id]', query: { id: dappId, url: portfolioUrl.toString() } }) }
+        { ...linkProps }
+      >
+        { buttonContent }
+      </LinkInternal>
+    ) : (
+      <LinkExternal
+        href={ portfolioUrl.toString() }
+        { ...linkProps }
+      >
+        { buttonContent }
+      </LinkExternal>
+    );
+  } catch (error) {}
+
+  return null;
+};
+
+export default AddressMultichainButton;