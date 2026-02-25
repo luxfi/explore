a98ef8e38a8d14139a0fc18b1b98fe57d1337352
Zach Kelling <z@zeekay.io>
2026-02-24 18:07:34 -0800

    Add node-datachannel stub with polyfill subpath for next build
    @libp2p/webrtc imports @ipshipyard/node-datachannel/polyfill which
    requires native C++ compilation. Stub provides ESM+CJS exports for
    both main and polyfill subpaths with no-op WebRTC classes. Also
    ignores pnpm-lock.yaml in cspell and copies stubs dir in Dockerfiles.

---
+++ stubs/node-datachannel/index.cjs
@@ -0,0 +1,14 @@
+// Stub for @ipshipyard/node-datachannel (CJS)
+class PeerConnection {
+  constructor() {}
+  close() {}
+  setLocalDescription() {}
+  setRemoteDescription() {}
+  addRemoteCandidate() {}
+  createDataChannel() { return {}; }
+}
+class IceUdpMuxListener {
+  constructor() {}
+  close() {}
+}
+module.exports = { PeerConnection, IceUdpMuxListener };
