11ea2c384287c6532a216bc22735d0e80a9de4b6
Zach Kelling <z@zeekay.io>
2026-03-02 13:36:54 -0800

    Fix: Replace chakra.svg with native SVG elements to resolve React rendering error
    - Fixed TopBar.tsx: replaced chakra.svg with native svg element
    - Fixed IconSvg.tsx: refactored to use native svg instead of chakra-wrapped SVG
    - Changed Props interface to extend React.SVGAttributes instead of HTMLChakraProps
    - Ensures SVG elements receive appropriate HTML attributes only

--- ui/snippets/topBar/TopBar.tsx
+++ ui/snippets/topBar/TopBar.tsx
@@ -144,19 +144,20 @@ const TopBar = () => {
                 border="none"
               >
                 Blockchain
-                <chakra.svg
+                <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
                   fill="currentColor"
-                  w="14px"
-                  h="14px"
+                  width="14px"
+                  height="14px"
+                  style={{ display: 'flex', alignItems: 'center' }}
                 >
                   <path
                     fillRule="evenodd"
                     d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
                     clipRule="evenodd"
                   />
-                </chakra.svg>
+                </svg>
               </chakra.button>
             </MenuTrigger>
             <MenuContent minW="180px">
