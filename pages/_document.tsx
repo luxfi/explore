8e8addca5491a46deeb8f90e9110340fa819e6bc
Hanzo AI <dev@hanzo.ai>
2026-04-06 09:10:55 -0700

    fix: add dark class to html element (fixes white popover/dropdown background)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -33,7 +33,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="en">
+      <Html lang="en" className="dark">
         <Head>
           { /* FONTS */ }
           <link
